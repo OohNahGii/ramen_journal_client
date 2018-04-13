e5a5e929754bee6c7dc0166e463195ec41d54cd4
OohNahGii <grantramil@gmail.com>
2018-04-12 17:58:08 -0700

    Add header and favicon with transparent background

--- src/App.vue
+++ src/App.vue
@@ -60,9 +60,6 @@ export default {
 </script>
 
 <style lang='scss'>
-@import url('https://fonts.googleapis.com/css?family=Oxygen');
-@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
-
 html {
   box-sizing: border-box;
 }
